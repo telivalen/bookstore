83a02a67b71c258b797ebcec8ccefb574727b4b2
lastname <telivalen@icloud.com>
2024-07-20 17:59:33 -0400

    book admin

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,7 +40,15 @@ INSTALLED_APPS = [
     #local
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
+    'books.apps.BooksConfig',
+    # third
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
