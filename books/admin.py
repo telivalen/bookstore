a7b7d44b819318d130890ac0ae6154804bd8bc3c
lastname <telivalen@icloud.com>
2024-08-10 18:13:14 -0400

    add reviews books

--- books/admin.py
+++ books/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Review
 
 # Register your models here.
+
+class ReviewInline(admin.TabularInline):
+    model = Review
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'price')
     list_filter = ('author', )
     search_fields = ('title', 'author')
     ordering = ('title', 'price', )
+    inlines = [ReviewInline,]
+
 
 admin.site.register(Book, BookAdmin)
